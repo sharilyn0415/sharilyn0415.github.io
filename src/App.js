a0121daeb588dc839488546b6fb162661a37ef70
Sharilyn <sharilyn0415@gmail.com>
2024-05-13 01:21:48 -0700

    fix(githubPage): fix gh pages with react route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
 import Header from "./pages/components/nav.jsx";
 import Home from "./pages/home.jsx";
 import Progress from "./pages/demo.jsx";
@@ -8,13 +8,13 @@ const App = () => {
   return (
     <div className="App">
       <Header />
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/progress" element={<Progress />} />
           <Route path="/*" element={<Navigate replace to="/" />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 };
